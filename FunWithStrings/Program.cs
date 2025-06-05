269adb64b11315c749a16df516d7564520f0d6db
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2025-06-05 17:57:44 +0200

    Created new console app called FunWithStrings and added it to the sln file.

---
+++ FunWithStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int index = 1;
+string name = "isac";
+char initial = 'i';
+int year = 1994;
+decimal height = 1.90m;
+bool doWeLoveToCode = true;
+
+string myParagraph = $@"These are the most common data types:
+{index} - string, example: {name}
+{++index} - char, example: {initial}
+{++index} - char, example: {initial}
+{++index} - int, example: {year}
+{++index} - decimal, height: {height}
+{++index} - boolean, doWeLoveToCode: {doWeLoveToCode}";
+
+Console.WriteLine(myParagraph);
+Console.ReadLine();
