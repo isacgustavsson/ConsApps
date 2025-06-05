d4609b957aa7ce8ea67071ca8c4c10907982390c
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2025-06-05 17:16:23 +0200

    Added sln file for reducing debugging noise in terminal

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -10,3 +10,20 @@ You should record previous games in a List and there should be an option in the 
 You don't need to record results on a database. Once the program is closed the results will be deleted.
 
 */
+int index = 1;
+string name = "isac";
+char initial = 'i';
+int year = 1994;
+decimal height = 1.90m;
+bool doWeLoveToCode = true;
+
+string myParagraph = $@"These are the most common data types:
+{index} - string, example: {name}
+{++index} - char, example: {initial}
+{++index} - char, example: {initial}
+{++index} - int, example: {year}
+{++index} - decimal, height: {height}
+{++index} - boolean, doWeLoveToCode: {doWeLoveToCode}";
+
+Console.WriteLine(myParagraph);
+Console.ReadLine();
